838caa1a4c6a7f4d3c4bcbfd949a0197752a7060
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2022-10-16 21:34:40 -0300

    Update main.py

--- Source/main.py
+++ Source/main.py
@@ -6,3 +6,10 @@ class menu(MDApp):
 
 if __name__ == "__main__":
     menu().run()
+
+def start():
+    option = "n"
+    while == "n"
+        option = input("quer criar uma conta para salvar?").lower()
+        if option != "s":
+            print("lol")
